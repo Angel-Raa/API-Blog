4abfe0fae0dcf5b63b1b23b3ca330b5c65b70c1b
Angel Aguero <angelagueror23@gmail.com>
2023-09-01 21:24:28 -0400

    reply message

--- src/main/java/org/caja/idea/utils/Message.java
+++ src/main/java/org/caja/idea/utils/Message.java
@@ -1,5 +1,16 @@
 package org.caja.idea.utils;
 
 public class Message {
+    public static final String USERNAME_REQUIRED = "Username is required";
+    public static final String EMAIL_REQUIRED = "Email is required";
+    private static final String EMAIL_NOT_VALID = "Email is not valid";
+    public static final String PASSWORD_REQUIRED = "Password is required";
+    public static final String PASSWORD_MIN_LENGTH = "Password must be at least 8 characters";
+    public static final String USERNAME_MIN_LENGTH = "Username must be at least 4 characters";
+    public static final String USERNAME_MAX_LENGTH = "Username must be at most 50 characters";
+    public static final String USERNAME_ALREADY_EXISTS = "Username already exists";
+    public static final String EMAIL_ALREADY_EXISTS = "Email already exists";
+    public static final String USER_NOT_FOUND = "User not found";
+
     
 }
