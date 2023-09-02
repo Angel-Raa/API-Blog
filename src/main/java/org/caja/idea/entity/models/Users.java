8117a9e46d9bfd3647f020aac364c9059b5e595d
Angel Aguero <angelagueror23@gmail.com>
2023-09-01 21:08:32 -0400

    Validaciones de users

--- src/main/java/org/caja/idea/entity/models/Users.java
+++ src/main/java/org/caja/idea/entity/models/Users.java
@@ -10,27 +10,29 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 import org.hibernate.annotations.CreationTimestamp;
 
 @Entity
 @Table(name = "users")
 public class Users {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(length = 50, unique= true)
+    @Column(length = 50, unique = true)
+    @Size(min = 4, max = 50, message = "username must be between 4 and 50 characters")
     @NotBlank(message = "username is required")
     private String username;
     @Email(message = "email is not valid")
     @NotBlank(message = "email is required")
-    @Column(length = 50, unique= true)
+    @Column(length = 50, unique = true)
     private String email;
     @NotBlank(message = "password is required")
+    @Size(min = 8, message = "password must be at least 8 characters")
     private String password;
     @CreationTimestamp
     @Column(name = "create_at", updatable = false)
     private LocalDateTime createAt;
 
-
-
-    
 }
